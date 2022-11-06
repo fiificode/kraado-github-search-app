6e4202240570973341e8654e260739296e7a5ebc
Franklin Mensah <fiificode@gmail.com>
2022-11-06 06:36:31 +0000

    final edit

--- main.js
+++ main.js
@@ -23,9 +23,9 @@ async function add_repo() {
   repos_container.innerHTML = reposData.map(repo => {
     return `
     <ul>
-      <a href="#"><li>${repo.name}</li></a>
+      <a href="${repo.html_url}" target="_blank"><li>${repo.name}</li></a>
+      <hr>
     </ul>
-
     `
   }).join('');
 }
@@ -96,126 +96,3 @@ btn.addEventListener('click',async() => {
 })
 
 
-// const API_LINK = 'https://api.github.com/users/';
-
-// let search_btn = document.querySelector('button');
-// const input = document.querySelector('input');
-// const card = document.querySelector('.card');
-// let repoUl = document.getElementById('repo');
-
-// var repo = new Array(0);
-
-// search_btn.addEventListener('click',(e)=>{
-//   e.preventDefault();
-//   if(input.value){
-//     getUserDetails(`${API_LINK}${input.value}`)
-//     getRepoDetails(`${API_LINK}${input.value}/repos`)
-//   }else{
-//     alert('Please enter any github username')
-//     input.focus();
-//   }
-// })
-
-
-// function showUserDetails(data){
-//   let card = document.querySelector('.card-body');
-
-//   var j =``;
-
-//   repo.forEach((r)=>{
-//     r.forEach((res)=>{
-//       j+= `<a href="${res.html_url}"><li>${res.name}</li></a>`;
-//     })
-//   })
-
-//   card.innerHTML = (`
-//       <div class="profile-container">
-//       <div class="profile-row">
-//         <div class="profile-image">
-//           <img src="${data.avatar_url}" alt="">
-//         </div>
-//         <div class="about">
-//           <div class="details">
-//               <h1 class="name">${data.name}</h1>
-//               <h3 class="username">
-//                 @ ${data.login}
-//               </h3>
-//               <p class="country">
-//                 <i class="fa fa-map-marker" aria-hidden="true"></i>
-//                 ${data.location}
-//               </p>
-//               <p class="twitter">
-//                 <i class="fa fa-twitter" aria-hidden="true"></i>
-//                 ${data.twitter_username}
-//               </p>
-//               <p class="date-created">
-//                 <i class="fa fa-calendar-o" aria-hidden="true"></i>
-//                 ${data.created_at}
-//               </p>
-//           </div>
-//           <div class="profile-btn">
-//             <a href="${data.html_url}" target="_blank">Visit Profile</a>
-//           </div>
-//         </div>
-//       </div>
-//       <div class="bio">
-//         <h3>About</h3>
-//         <p>${data.bio}</p>
-//       </div>
-//       <div class="follows-row">
-//         <div class="follows-col">
-//           <h3 class="heading">Followers</h3>
-//           <p>${data.followers}</p>
-//         </div>
-//         <div class="follows-col">
-//           <h3 class="heading">Following</h3>
-//           <p>${data.following}</p>
-//         </div>
-//         <div class="follows-col">
-//           <h3 class="heading">Repos</h3>
-//           <p>${data.public_repos}</p>
-//         </div>
-//       </div>
-//       <h3 class="repos-heading">
-//         Repositories
-//       </h3>
-//       <div class="repos-container">
-//         <ul id="repo">
-//           ${j}
-//         </ul>
-//       </div>
-//     </div> 
-//   `);
-// }
-
-
-
-// async function getUserDetails(api){
-//   let query = await fetch(api)
-//   .then(async (query)=> {
-//     return await query.json();
-//   })
-//   .then((result)=>{
-//     if(!result.name){
-//       alert("User not found");
-//       location.reload();
-//     }else{
-//       console.log(result);
-//       showUserDetails(result);
-//     }
-//   }).catch((error)=>{
-//     console.log(error)
-//   })
-// }
-
-// async function getRepoDetails(repo_api){
-//   let repo_query = await fetch(repo_api)
-//   .then(async (repo_query) => {
-//       return await repo_query.json();
-//   })
-//   .then((repo_result)=>{
-//     repo.push(repo_result); 
-//   }).catch((error)=>{
-//       console.log(error);
-//   })
-// }
